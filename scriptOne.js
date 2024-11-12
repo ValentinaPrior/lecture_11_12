eab8811e692372e896c6863bb4e33ddb3bd7adf6
ValentinaPrior <vbp7@njit.edu>
2024-11-12 10:31:23 -0500

    adding variables

--- scriptOne.js
+++ scriptOne.js
@@ -11,8 +11,28 @@ let rollNo= 21;
 rollNo=101;
 
 
-
-
-
-
 console.log(rollNo);
+
+const valuePi = 3.14;
+
+console.log(valuePi);
+
+
+
+
+var grade= 80;
+grade=85;
+console.log(grade);
+
+let gradeOne= 42;
+console.log(gradeOne);
+
+const gradeTwo= 99;
+console.log(gradeTwo);
+
+
+let age2=20;
+console.log(age2);
+
+let userName= "Harry";
+console.log(userName);
